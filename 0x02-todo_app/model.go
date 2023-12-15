264b088fc3d772f580d82ec9b27490bad29bafa9
brk-a <fnjakai@outlook.com>
2023-12-15 07:06:31 +0300

    CRUD

--- 0x02-todo_app/model.go
+++ 0x02-todo_app/model.go
@@ -6,14 +6,14 @@ type Todo struct {
 	Done bool `json:"done"`
 }
 
-func CreateTodo(todo string) err {
+func CreateTodo(todo string) error {
 	statement := `insert into todos(todo, done) values($1, $2);`
 	_, err := db.Query(statement, todo, false)
 
 	return err
 }
 
-func GetAllTodos()([]Todo, err){
+func GetAllTodos()([]Todo, error){
 	todos := []Todo{}
 	statement := `select * from todos;`
 
@@ -31,12 +31,74 @@ func GetAllTodos()([]Todo, err){
 		var id uint64
 
 		err := db.Scan(&id, &title, &done)
+		if err!=nil {
+			return todos, err
+		}
+
 		todo := Todo{
 			Id: id,
 			Todo: todo,
 			Done: done,
 		}
-
 		todos = append(todos, todo)
 	}
+
+	return todos, err
+}
+
+func GetTodo(id uint64) (Todo, error) {
+	statement := `select *todo, done from todos where id=$1;`
+	todo := Todo{}
+	todo.Id = id
+
+	row, err := db.Query(statement, id)
+	if err!=nil {
+		return todo, err
+	}
+
+	for row.Next() {
+		var title string
+		var done bool
+
+		error := row.Scan(&title, &done)
+		if err!=nil {
+			return todo, err
+		}
+
+		todo.Todo = title
+		todo.Done = done
+	}
+
+	return todo, err
+}
+
+func UpdateTodo(id uint64, item string) error {
+	todo, err := GetTodo(id)
+	if err!=nil {
+		return err
+	}
+
+	statement = `update todos set todo=$2 where id=$1;`
+	_, err := db.Query(statement, id, item)
+
+	return err
+}
+
+func MarkDone(id uint64) error {
+	todo, err := GetTodo(id)
+	if err!=nil {
+		return err
+	}
+
+	statement := `update todos set done=$2 where id=$1;`
+	_, err := db.Query(statement, id, !todo.Done)
+
+	return err
+}
+
+func DeleteTodo(id uint64) error {
+	statement := `delete from todos where id=$1;`
+	_, err := db.Exec(statement, id)
+
+	return err
 }
