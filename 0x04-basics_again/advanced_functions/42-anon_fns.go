bfea37696bc9385f07d8b5d526cffd52862777ab
brk-a <fnjakai@outlook.com>
2024-01-04 04:39:01 +0300

    closures

---
+++ 0x04-basics_again/advanced_functions/42-anon_fns.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func printReports(message []string) {}
+
+func test(messages []string) {
+	defer fmt.Println("========================================================")
+	printReports(messages)
+}
+
+func main() {
+	test([]string{
+		"Here's Goat Matata",
+		"He owns and runs 'The GOAT Podcast'",
+	})
+	test([]string{
+		"Here's Dada Ng'ombe",
+		"She owns and runs 'Chizi About Cheese'",
+	})
+	test([]string{
+		"Here's Paka the Cat",
+		"He owns and runs 'Catnip & Chill'",
+	})
+}
+
+func printCostReport(costCalculator func(string) int, message string) {
+	cost := costCalculator(message)
+	fmt.Printf(`Message: [%s] Cost: Kes %v`, message, cost)
+	fmt.Println()
+}
