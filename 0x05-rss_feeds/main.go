7edf4b8c5e55b4efed0271e2dc06caa9c38b6f09
brk-a <fnjakai@outlook.com>
2024-01-14 06:42:41 +0300

    add route

--- 0x05-rss_feeds/main.go
+++ 0x05-rss_feeds/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/cors"
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
-
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
@@ -18,5 +18,23 @@ func main() {
 		log.Fatal("port could not be found in env")
 	}
 
-	fmt.Println("Port: ", portString)
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":"+portString,
+	}
+	log.Printf("server starting at port %v", portString)
+	err := srv.ListenAndServe()
+	if err!=nil {
+		log.Fatal(err)
+	}
 }
