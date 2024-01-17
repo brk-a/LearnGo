83491f13d9933b4b390a34add2b2034086d8fc19
brk-a <fnjakai@outlook.com>
2024-01-17 04:54:21 +0300

    user handler and model

--- 0x05-rss_feeds/main.go
+++ 0x05-rss_feeds/main.go
@@ -52,6 +52,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerError)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
