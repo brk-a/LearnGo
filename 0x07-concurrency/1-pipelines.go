dd514fab10aca5b892d5f2df896589e2e13785b3
brk-a <fnjakai@outlook.com>
2024-02-03 04:41:51 +0300

    pipelines

--- 0x07-concurrency/1-pipelines.go
+++ 0x07-concurrency/1-pipelines.go
@@ -3,7 +3,36 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
+
+func primeFinder(done <-chan int, randIntStream <-chan int) <-chan int {
+	primes := make(chan int)
+	isPrime := func (randInt int) bool {
+		for i:=randInt-1; i>1; i-- {
+			if randInt%i==0 {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	go func() {
+		defer close(primes)
+
+		select {
+		case <-done:
+			return
+		case randInt:= <-randIntStream:
+			if isPrime(randInt) {
+				primes <-randInt
+			}
+		}
+	}()
+
+	 return primes
+}
 
 func take[T any, K any](done <-chan K, stream <-chan T, n  int) <-chan T {
 	taken := make(chan T)
@@ -23,12 +52,16 @@ func take[T any, K any](done <-chan K, stream <-chan T, n  int) <-chan T {
 }
 
 func runTake() {
+	start := time.Now()
 	done := make(chan int)
 	defer close(done)
 
 	randNumFetcher := func() int {return rand.Intn(1000000000)}
 	randNumStream := generator(done, randNumFetcher)
-	for i:=range take(done, randNumStream, 20) {
+	primesStream := primeFinder(done, randNumStream)
+	for i:=range take(done, primesStream, 20) {
 		fmt.Println(i)
 	}
+
+	fmt.Printf("operation takes %v", time.Since(start))
 }
