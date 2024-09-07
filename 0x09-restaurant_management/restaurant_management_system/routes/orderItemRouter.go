7937abad3e76deed854135864bdf681502571cae
brk-a <fnjakai@outlook.com>
2024-09-07 13:54:08 +0300

    routes

--- 0x09-restaurant_management/restaurant_management_system/routes/orderItemRouter.go
+++ 0x09-restaurant_management/restaurant_management_system/routes/orderItemRouter.go
@@ -12,7 +12,7 @@ func OrderItemRoutes(incomingRoutes *gin.Engine) {
     incomingRoutes.POST("/orderItems", controller.CreateOrderItem())
     incomingRoutes.PATCH("/orderItems/:orderItem_id", controller.UpdateOrderItem())
     // incomingRoutes.DELETE("/orderItems/:orderItem_id", controller.DeleteOrderItem())
-    // incomingRoutes.GET("/orderItems/order/:order_id", controller.GetOrderItemsByOrder())
-    // incomingRoutes.GET("/orderItems/food/:food_id", controller.GetOrderItemsByFood())
-    // incomingRoutes.GET("/orderItems/quantity/:quantity", controller.GetOrderItemsByQuantity())
+    incomingRoutes.GET("/orderItems-order/:order_id", controller.GetOrderItemsByOrder())
+    // incomingRoutes.GET("/orderItems-food/:food_id", controller.GetOrderItemsByFood())
+    // incomingRoutes.GET("/orderItems-quantity/:quantity", controller.GetOrderItemsByQuantity())
 }
