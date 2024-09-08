35a743c983618cafa6ae71039c957a763f5ec6fa
brk-a <fnjakai@outlook.com>
2024-09-08 11:07:40 +0300

    controllers

---
+++ 0x09-restaurant_management/restaurant_management_system/controllers/foodController.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetFoods() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func GetFood() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func CreateFood() gin.HandlerFunc {
+    return func(c *gin.Context) {}
+}
+
+func UpdateFood() gin.HandlerFunc {
+    return func(c *gin.Context) {}
+}
+
+func round(num float64) int {
+	return int(num); 
+}
+
+func toFixed(num float64, precision int) float64 {
+	return float64(num);
+}
---
+++ 0x09-restaurant_management/restaurant_management_system/controllers/userController.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func SignIn() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func HashPassword(password string) string {
+	return "";
+}
+
+func VerifyPassword(userPassword, hashedPassword string) bool {
+    return false;
+}
