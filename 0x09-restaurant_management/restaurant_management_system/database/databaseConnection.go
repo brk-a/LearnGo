83e1e1aca0ad5f125d8198e6405a491a80ab0f43
brk-a <fnjakai@outlook.com>
2024-09-09 11:19:09 +0300

    db conn

--- 0x09-restaurant_management/restaurant_management_system/database/databaseConnection.go
+++ 0x09-restaurant_management/restaurant_management_system/database/databaseConnection.go
@@ -33,7 +33,7 @@ func DBinstance() *mongo.Client {
 	return client;
 }
 
-var CLient *mongo.Client = DBinstance()
+var Client *mongo.Client = DBinstance()
 
 func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
 	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName);
