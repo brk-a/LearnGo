f287c118c52e5f668f4e03818c5580bab39face0
brk-a <fnjakai@outlook.com>
2024-09-09 11:18:36 +0300

    edit tableRouter

--- 0x09-restaurant_management/restaurant_management_system/routes/tableRouter.go
+++ 0x09-restaurant_management/restaurant_management_system/routes/tableRouter.go
@@ -8,13 +8,13 @@ import (
 
 func TableRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/tables", controller.GetTables())
-    incomingRoutes.GET("/tables/:table_id", controller.GetTable())
-    incomingRoutes.POST("/tables", controller.CreateTable())
-    incomingRoutes.PATCH("/tables/:table_id", controller.UpdateTable())
-    // incomingRoutes.DELETE("/tables/:table_id", controller.DeleteTable())
-    // incomingRoutes.GET("/tables/restaurant/:restaurant_id", controller.GetTablesByRestaurant())
-    // incomingRoutes.GET("/tables/status/:status", controller.GetTablesByStatus())
-    // incomingRoutes.GET("/tables/capacity/:capacity", controller.GetTablesByCapacity())
-    // incomingRoutes.GET("/tables/waiter/:waiter_id", controller.GetTablesByWaiter())
-    // incomingRoutes.GET("/tables/customer/:customer_id", controller.GetTablesByCustomer())
+	incomingRoutes.GET("/tables/:table_id", controller.GetTable())
+	incomingRoutes.POST("/tables", controller.CreateTable())
+	incomingRoutes.PATCH("/tables/:table_id", controller.UpdateTable())
+	// incomingRoutes.DELETE("/tables/:table_id", controller.DeleteTable())
+	// incomingRoutes.GET("/tables/restaurant/:restaurant_id", controller.GetTablesByRestaurant())
+	// incomingRoutes.GET("/tables/status/:status", controller.GetTablesByStatus())
+	// incomingRoutes.GET("/tables/capacity/:capacity", controller.GetTablesByCapacity())
+	// incomingRoutes.GET("/tables/waiter/:waiter_id", controller.GetTablesByWaiter())
+	// incomingRoutes.GET("/tables/customer/:customer_id", controller.GetTablesByCustomer())
 }
