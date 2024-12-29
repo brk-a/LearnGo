6ee05b7c1b325cd4e2508d9c1dd1a07a2cd7ef79
brk-a <fnjakai@outlook.com>
2024-12-29 07:58:43 +0300

    orderController

--- 0x09-restaurant_management/restaurant_management_system/controllers/orderController.go
+++ 0x09-restaurant_management/restaurant_management_system/controllers/orderController.go
@@ -12,7 +12,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"gopkg.in/mgo.v2/bson"
+    "go.mongodb.org/mongo-driver/bson"
 )
 
 var orderCollection *mongo.Collection = database.OpenCollection(database.Client, "order")
@@ -37,6 +37,7 @@ func GetOrders() gin.HandlerFunc {
             return
         }
 
+        defer cancel()
         c.JSON(http.StatusOK, allOrders)
     }
 }
@@ -44,7 +45,7 @@ func GetOrders() gin.HandlerFunc {
 func GetOrder() gin.HandlerFunc {
     return func(c *gin.Context) {
         ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
-        orderId := c.Param(":order_id")
+        orderId := c.Param("order_id")
         var order models.Order
 
         err := orderCollection.FindOne(ctx, bson.M{"order_id": orderId}).Decode(&order)
@@ -55,12 +56,54 @@ func GetOrder() gin.HandlerFunc {
             return
         }
 
+        defer cancel()
         c.JSON(http.StatusOK, order)
     }
 }
 
 func CreateOrder() gin.HandlerFunc {
-    return func(c *gin.Context) {}
+    return func(c *gin.Context) {
+        ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+        var order models.Order
+        var table models.Table
+
+        if err:=c.BindJSON(&order); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        }
+
+        validationError := validate.Struct(order)
+        defer cancel()
+        if validationError!=nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": validationError.Error()})
+            return
+        }
+
+        if order.Table_id!=nil {
+            err := tableCollection.FindOne(ctx, bson.M{"table_id": order.Table_id}).Decode(&table)
+            defer cancel()
+            if err!=nil {
+                msg := fmt.Sprintf("error finding table that placed order")
+                c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+                return
+            }
+        }
+
+        order.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+        order.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+        order.ID = primitive.NewObjectID()
+        order.Order_id = order.ID.Hex()
+
+        result, err := orderCollection.InsertOne(ctx, order)
+        defer cancel()
+        if err!= nil {
+            msg := fmt.Sprintf("error creating order")
+            c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+            return
+        }
+
+        defer cancel()
+        c.JSON(http.StatusOK, gin.H{"order": result})
+    }
 }
 
 func UpdateOrder() gin.HandlerFunc {
@@ -69,12 +112,13 @@ func UpdateOrder() gin.HandlerFunc {
         var order models.Order
         var table models.Table
 
+        orderId := c.Param("order_id")
+        defer cancel()
         if err := c.BindJSON(&order); err!=nil {
             c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
             return
         }
 
-        orderId := c.Param(":order_id")
         filter := bson.M{"order_id": orderId}
         var updateObj primitive.D
 
@@ -113,5 +157,44 @@ func UpdateOrder() gin.HandlerFunc {
 }
 
 func DeleteOrder() gin.HandlerFunc {
-    return func(c *gin.Context) {}
+    return func(c *gin.Context) {
+        ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+        orderId := c.Param("order_id")
+        filter := bson.M{"order_id": orderId}
+
+        result, err := orderCollection.DeleteOne(ctx, filter)
+        defer cancel()
+        if err!= nil {
+            msg := fmt.Sprintf("error deleting order")
+            c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+            return
+        }
+
+        if result.DeletedCount == 0 {
+            c.JSON(http.StatusNotFound, gin.H{"error": "order not found"})
+            return
+        }
+
+        defer cancel()
+        c.JSON(http.StatusOK, gin.H{"message": "order deleted successfully"})
+    }
+}
+
+func OrderItemOrderCreator(order models.Order) string {
+    ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+    order.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+    order.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+    order.ID = primitive.NewObjectID()
+    order.Order_id = order.ID.Hex()
+
+    _, err := orderCollection.InsertOne(ctx, order)
+    defer cancel()
+    if err!= nil {
+        msg := fmt.Sprintf("error creating order")
+        return msg
+    }
+    
+    defer cancel()
+    return order.Order_id
 }
