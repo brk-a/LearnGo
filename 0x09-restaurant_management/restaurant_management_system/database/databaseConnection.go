eba05a464ec3732ad1d1d51d7d2cd31fafd17582
brk-a <fnjakai@outlook.com>
2024-12-29 07:58:55 +0300

    databaseConnection

--- 0x09-restaurant_management/restaurant_management_system/database/databaseConnection.go
+++ 0x09-restaurant_management/restaurant_management_system/database/databaseConnection.go
@@ -7,7 +7,7 @@ import (
 	"time"
 	"os"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongo.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func DBinstance() *mongo.Client {
