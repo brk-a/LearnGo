f1117204bdc791a6fd7d54e66b9c5e835b53c6b0
brk-a <fnjakai@outlook.com>
2025-01-03 10:11:54 +0300

    userController

--- 0x09-restaurant_management/restaurant_management_system/controllers/userController.go
+++ 0x09-restaurant_management/restaurant_management_system/controllers/userController.go
@@ -1,8 +1,15 @@
 package controllers
 
 import (
+	"restaurant_management_system/database"
+	"restaurant_management_system/models"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+var user models.User
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 
 func GetUsers() gin.HandlerFunc {
 	return func(c *gin.Context) {}
@@ -13,7 +20,23 @@ func GetUser() gin.HandlerFunc {
 }
 
 func SignUp() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		// TODO convert JSON data coming from DB/postman to sth Go understands
+		// TODO validate data based on user struct
+		// TODO check if email already exists
+		// TODO check if phone number already exists
+        // TODO generate verification code
+        // TODO send verification code to user's email or phone number
+        // TODO validate verification code from user's input
+        // TODO update user's status to verified
+        // TODO generate JWT token -> GenerateToken fn in helpers
+        // TODO return JWT token
+        // TODO hash password
+		// TODO add metadata -> ID, updated_at, etc
+        // TODO create new user in the DB
+        // TODO send confirmation email
+        // TODO return success message
+	}
 }
 
 func SignIn() gin.HandlerFunc {
@@ -21,9 +44,9 @@ func SignIn() gin.HandlerFunc {
 }
 
 func HashPassword(password string) string {
-	return "";
+	return ""
 }
 
-func VerifyPassword(userPassword, hashedPassword string) bool {
-    return false;
+func VerifyPassword(userPassword string, providePassword string) (bool, string) {
+	return false, ""
 }
