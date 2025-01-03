82d94b10e5dec5104c0560dc055fb3a161726334
brk-a <fnjakai@outlook.com>
2025-01-03 10:12:21 +0300

    authMiddleware

--- 0x09-restaurant_management/restaurant_management_system/middleware/authMiddleware.go
+++ 0x09-restaurant_management/restaurant_management_system/middleware/authMiddleware.go
@@ -1,6 +1,9 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import ("github.com/gin-gonic/gin"
+	// helpers "restaurant_management_system/helpers"
+)
+
 
 func Authentication() gin.HandlerFunc {
 	return func(c *gin.Context) {}
