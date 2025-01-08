ec9c0a5f29a2632bd6ce63410e51d7c8091a8d42
brk-a <fnjakai@outlook.com>
2025-01-08 08:57:30 +0300

    main

--- 0x0B-task_manager/main.go
+++ 0x0B-task_manager/main.go
@@ -1,89 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/brk-a/task_manager/routes"
+	"github.com/brk-a/task_manager/database"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type User struct {}
 
-type Todo struct {
-	ID int64 `json:"id"`
-	Title string `json:"title"`
-	Body string `json:"body"`
-	Completed bool `json:"completed"`
-	Duration int64 `json:"duration"`
-	// Created_at int64 `json:"created_at"`
-	// Updated_at int64 `json:"updated_at"`
-	// Deleted_at int64 `json:"deleted_at"`
-	// User User `json:"user"`
-	Due_date int64 `json:"due_date"`
-}
+var taskCollection *mongo.Collection
+func main() {
+	err := godotenv.Load(".env")
+	if err!= nil {
+        log.Fatal("Error loading.env file")
+		return
+    }
 
-func main(){
-	app := fiber.New()
+	client, dbErr := database.Database()
+	if dbErr!=nil {
+        log.Fatal("error initialising DB", err)
+		return
+    }
 
-	todos := []Todo{}
-
-	// routes
-	app.Get("/api/tasks", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{
-			"msg": "OK",
-			"data": todos,
-		})
-	})
-	app.Get("/api/tasks:id", func(c *fiber.Ctx) error {
-		todoId := c.Params("id")
-		for _, todo  := range(todos){
-			if fmt.Sprint(todo.ID) == todoId {
-                return c.Status(200).JSON(fiber.Map{
-                    "msg": "OK",
-                    "data": todo,
-                })
-            }
-		}
-		return c.Status(500).JSON(fiber.Map{"error": "could not retrieve"})
-	})
-	app.Post("/api/tasks", func (c *fiber.Ctx) error {
-		todo := &Todo{}
-		if err := c.BodyParser(todo); err!= nil {
-            return c.Status(500).JSON(fiber.Map{"error": err.Error()})
-        }
-		if todo.Body == "" {
-			return c.Status(400).JSON(fiber.Map{"error": "Body must be a string"})
-		}
-
-		todo.ID = int64(len(todos)) + 1
-		todos = append(todos, *todo)
-		
-		return c.Status(201).JSON(fiber.Map{"msg": "created successfully"})
-		
-	})
-	app.Patch("/api/tasks:id", func (c *fiber.Ctx) error  {
-		todoId := c.Params("id")
-		for i, todo  := range(todos){
-			if fmt.Sprint(todos[i].ID) == todoId {
-                if err := c.BodyParser(todo); err!= nil {
-                    return c.Status(500).JSON(fiber.Map{"error": err.Error()})
-                }
-                todos[i].Completed = !todos[i].Completed
-                return c.Status(200).JSON(fiber.Map{"msg": "updated successfully"})
-            }
-		}
-		return c.Status(404).JSON(fiber.Map{"error": "unable to update"})
-	})
-	app.Delete("/api/tasks/:id", func(c *fiber.Ctx) error {
-		taskId := c.Params("id")
-		for i, todo  := range(todos){
-			if fmt.Sprint(todo.ID) == taskId {
-                todos = append(todos[:i], todos[i+1:]...)
-                return c.Status(204).JSON(fiber.Map{"msg": "deleted successfully"})
-            }
-		}
-		return c.Status(500).JSON("error", "could not delete")
-	})
-
-	log.Fatal(app.Listen(":8080"))
+	taskCollection = client.Database("task_manager_db").Collection("tasks")
+	routes.Routes()	
 }
