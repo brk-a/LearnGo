af062291ee2279bd8f589e1f69aaa2cf99dc8f67
brk-a <fnjakai@outlook.com>
2025-01-08 08:57:19 +0300

    controllers

---
+++ 0x0B-task_manager/controllers/taskController.go
@@ -0,0 +1,9 @@
+package controllers
+
+import "github.com/gofiber/fiber"
+
+func GetTasks(c *fiber.Ctx){}
+func GetTask(c *fiber.Ctx){}
+func CreateTask(c *fiber.Ctx){}
+func UpdateTask(c *fiber.Ctx){}
+func DeleteTask(c *fiber.Ctx){}
