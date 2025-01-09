7b443fe5ee65a3c23a8edbf2f77b5c7f6bc5ed12
brk-a <fnjakai@outlook.com>
2025-01-09 08:36:02 +0300

    models

--- 0x0B-task_manager/models/taskModel.go
+++ 0x0B-task_manager/models/taskModel.go
@@ -1,8 +1,18 @@
 package models
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Task represents a task with an ID, title, body and completion status.
 type Task struct {
-	ID int64 `json:"_id" bson:"_id"`
-	Title string `json:"title"`
-	Body string `json:"body"`
-	Completed bool `json:"completed"`
+	ID         primitive.ObjectID `json:"_id" bson:"_id"`            // Unique identifier for the task.
+	Title      string             `json:"title" validate:"required"` // Title of the task.
+	Body       string             `json:"body" validate:"required"`  // Description of the task.
+	Completed  bool               `json:"completed"`                 // Indicates whether the task is completed.
+	Task_id    string             `json:"task_id"`                   //
+	Created_at time.Time          `json:"created_at"`                // Date and time when the task was created.
+	Updated_at time.Time          `json:"updated_at"`                // Date and time when the task was last updated.
 }
