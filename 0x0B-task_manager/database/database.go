b96d6909ce3c88937f854f211dcc05c4213c62e9
brk-a <fnjakai@outlook.com>
2025-01-09 08:36:13 +0300

    database

--- 0x0B-task_manager/database/database.go
+++ 0x0B-task_manager/database/database.go
@@ -11,6 +11,8 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// Database establishes a connection to the MongoDB database using the provided URI.
+// It returns a pointer to the connected client and an error if any occurs during the connection process.
 func Database() (client *mongo.Client, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	MONGODB_URI := os.Getenv("MONGODB_URI")
@@ -27,7 +29,42 @@ func Database() (client *mongo.Client, err error) {
 		log.Panic(err)
 		return nil, err
 	}
-	
+
 	fmt.Println("connected to mongoDB atlas...")
 	return client, nil
+}
+
+func createMongoClient() (*mongo.Client, error) {
+	mongoClient, dbErr := Database()
+	if dbErr != nil {
+		log.Fatal("error initialising DB", dbErr)
+		return nil, dbErr
+	}
+
+	return mongoClient, nil
+}
+
+var Client, err = Database()
+
+// CloseDB closes the connection to the MongoDB database.
+// It takes a pointer to the MongoDB client as a parameter and returns an error if any occurs during the disconnection process.
+func CloseDB(client *mongo.Client) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err := client.Disconnect(ctx)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	fmt.Println("disconnected from MongoDB Atlas...")
+	return nil
+}
+
+// GetCollection returns a reference to the specified collection in the connected MongoDB database.
+// It takes a pointer to the MongoDB client and the name of the collection as parameters.
+// The function also calls CloseDB to close the connection after retrieving the collection reference.
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection = client.Database("tasks").Collection(collectionName)
+	defer CloseDB(client)
+	return collection
 }
