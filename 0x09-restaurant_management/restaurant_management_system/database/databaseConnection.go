7e91ce827168202c2e1205619a8f7a72f70a3b36
brk-a <fnjakai@outlook.com>
2025-01-27 10:45:16 +0300

    connect to DB

--- 0x09-restaurant_management/restaurant_management_system/database/databaseConnection.go
+++ 0x09-restaurant_management/restaurant_management_system/database/databaseConnection.go
@@ -1,42 +1,50 @@
 package database
 
 import (
-    "context"
-    "fmt"
-    "log"
-	"time"
+	"context"
+	"fmt"
+	"log"
 	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func DBinstance() *mongo.Client {
-	MongoDb := "mongodb://localhost:" + os.Getenv("MONGODB_PORT"); //27107
-	fmt.Println(MongoDb);
+	// MongoDb := "mongodb://localhost:" + os.Getenv("MONGODB_PORT") //27107
+	// fmt.Println(MongoDb)
+	err := godotenv.Load()
+	if err!=nil{
+			log.Fatal("Error loading .env file")
+	}
+	MONGODB_URI := os.Getenv("MONGODB_URI")
 
 	// TODO: mongo.NewClient is deprecated: Use [Connect] instead
-	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb));
+	client, err := mongo.NewClient(options.Client().ApplyURI(MONGODB_URI))
 	if err != nil {
-		log.Fatal(err);
+		log.Fatal(err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second);
-	defer cancel();
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-	err = client.Connect(ctx);
+	err = client.Connect(ctx)
 	if err != nil {
-		log.Fatal(err);
+		log.Fatal(err)
 	}
 
-	fmt.Println("connected to DB");
+	fmt.Println("connected to DB")
 
-	return client;
+	return client
 }
 
 var Client *mongo.Client = DBinstance()
 
 func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName);
+	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
 
-	return collection;
+	return collection
 }
