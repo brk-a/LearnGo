226de07899d38b97ef1039d8692ad6aad844eb5c
brk-a <fnjakai@outlook.com>
2025-05-27 08:47:52 +0300

    main

--- 0x0C-offensive_go/main.go
+++ 0x0C-offensive_go/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	port_scanner "github.com/brk-a/offensive_go/port_scanner"
 	remote_shell "github.com/brk-a/offensive_go/remote_shell"
+	advanced_port_scanner "github.com/brk-a/offensive_go/advanced_port_scanner"
 )
 
 func main(){
 	port_scanner.Main()
 	remote_shell.Main()
+	advanced_port_scanner.Main()
 }
