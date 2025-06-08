febb7ada7dd317b192d4e388bf0ecc14c2c28dc1
brk-a <fnjakai@outlook.com>
2025-06-08 09:06:04 +0300

    main

--- 0x0C-offensive_go/main.go
+++ 0x0C-offensive_go/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	port_scanner "github.com/brk-a/offensive_go/port_scanner"
-	remote_shell "github.com/brk-a/offensive_go/remote_shell"
 	advanced_port_scanner "github.com/brk-a/offensive_go/advanced_port_scanner"
-	sniff_n_capture "github.com/brk-a/offensive_go/sniff_and_capture"
-	key_logger "github.com/brk-a/offensive_go/web_key_logger"
-	sub_domain_finder "github.com/brk-a/offensive_go/sub_domain_finder"
 	net_cred_sniffer "github.com/brk-a/offensive_go/network_credentials_sniffer"
+	port_scanner "github.com/brk-a/offensive_go/port_scanner"
+	remote_admin_tool "github.com/brk-a/offensive_go/remote_administration_tool"
+	remote_shell "github.com/brk-a/offensive_go/remote_shell"
+	sniff_n_capture "github.com/brk-a/offensive_go/sniff_and_capture"
+	sub_domain_finder "github.com/brk-a/offensive_go/sub_domain_finder"
+	key_logger "github.com/brk-a/offensive_go/web_key_logger"
 )
 
-func main(){
+func main() {
 	port_scanner.Main()
 	remote_shell.Main()
 	advanced_port_scanner.Main()
@@ -18,4 +19,5 @@ func main(){
 	key_logger.Main()
 	sub_domain_finder.Main()
 	net_cred_sniffer.Main()
+	remote_admin_tool.Main()
 }
